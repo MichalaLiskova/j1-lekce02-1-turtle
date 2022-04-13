56a1bf1681f6b25624c62afdd7c41d3a7f07f2b1
Michala Lišková <michala.liskova@email.cz>
2022-04-13 20:06:22 +0200

    želva vytvořila domečky vedle sebe

--- src/main/java/net/sevecek/turtle/HlavniProgram.java
+++ src/main/java/net/sevecek/turtle/HlavniProgram.java
@@ -9,7 +9,27 @@ public class HlavniProgram {
 
         zofka = new Turtle();
 
-        zofka.move(200);
+        for (int i = 0; i < 5; i++) {
+            zofka.turnLeft(30);
+            zofka.move(100);
+            zofka.turnLeft(120);
+            zofka.move(100);
+            zofka.turnLeft(30);
+            zofka.move(100);
+            zofka.turnLeft(90);
+            zofka.move(100);
+            zofka.turnLeft(90);
+            zofka.move(100);
+            zofka.turnLeft(90);
+            zofka.move(100);
+
+
+            zofka.penUp();
+            zofka.turnRight(180);
+            zofka.move(250);
+            zofka.turnLeft(90);
+            zofka.penDown();
+        }
     }
 
-}
+    }
